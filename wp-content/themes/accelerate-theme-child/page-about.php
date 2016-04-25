3cfece76db3d8fb5ec8e1213d92c5058876be9e7
Karen Storr <karenstorr@hotmail.com>
2016-04-25 11:52:46 -0400

    updating about page

--- wp-content/themes/accelerate-theme-child/page-about.php
+++ wp-content/themes/accelerate-theme-child/page-about.php
@@ -12,39 +12,40 @@
 
 get_header(); ?>
 
-	<section class="about-page">
+	<section class="home-page">
 			<div class="site-content">
 		<?php while ( have_posts() ) : the_post(); ?>
 			<div class='homepage-hero'>
-				<?php the_content();?>
+				<?php the_content(); ?>
 			</div>
-				</div><!-- .container -->
-				</section><!-- .about-page -->
+		<?php endwhile ?>
+			</div><!-- .container -->
+	</section><!-- .about-page -->
 	
-	
-			
-		<?php while ( have_posts() ) : the_post();
-				$service_description = get_field('service_description);
-				$icon = get_field('icon');?>
-				
-					
+	<div class ="services">
 		<ul class = "about-page-services">	
-				<h4><?php the_title(); ?></h4>
-				
-				<p><?php echo $service_description?></p>
-    	</ul>
+		<?php query_posts('post_type=services'); ?>
+			<?php while ( have_posts() ) : the_post (); 
+				$service_description = get_field('service_description');
+				$icon = get_field('icon'); 
+			?>
+			
+			<h4><?php the_title(); ?></h4>
+			<p><?php echo $service_description?></p>
+			</ul>
+	   
 		
 	<div class="services-icons">
   	  	<?php if($icon) { 
   			 echo wp_get_attachment_image( $icon, $size );
-  	
-		 } ?>
+  			 } ?>
+	</div>	
 	
-    </div>	
+		 <?php endwhile ?>
+		 <?php wp_reset_query(); ?>
+    
 
-	
-				
-			<?php endwhile; // end of the loop. ?>
+	</div>
 
 		
 <?php get_footer(); ?>
